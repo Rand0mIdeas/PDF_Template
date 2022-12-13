24725a2d365bec8b61fecce512389d97727420ba
Grant Kemm <grant.kemm0@gmail.com>
2022-12-13 14:46:54 +0200

    output correct num pages for pdf

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fpdf import FPDF
 import pandas as pd
 
-pdf = FPDF(orientation="P", unit="mm", format="A4")
 df = pd.read_csv("topics.csv")
+pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 for index, row in df.iterrows():
     pdf.add_page()
@@ -11,5 +11,9 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10, 20, 200, 20)
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+
 
 pdf.output("output.pdf")
